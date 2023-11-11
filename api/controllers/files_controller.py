8977bdce4a114ce02e0e1b4152724c051ec6b66a
semaan <toninsemaan@gmail.com>
2023-11-11 15:45:42 +0200

    fix paths for windows

--- api/controllers/files_controller.py
+++ api/controllers/files_controller.py
@@ -11,7 +11,7 @@ router = APIRouter()
 @router.post("/uploadfiles")
 async def create_upload_files(files: List[UploadFile] = File(...)):
     for file in files:
-        file_path = os.path.join("./data/uploaded_files", file.filename)
+        file_path = os.path.join(".\\data\\uploaded_files", file.filename)
         with open(file_path, "wb") as f:
             shutil.copyfileobj(file.file, f)
     return {"file_names": [file.filename for file in files]}
